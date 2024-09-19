eff95543abe9073988a264861098a220d34eae1a
indgy <65541311+indgy@users.noreply.github.com>
2024-09-19 17:34:38 +0100

    Removed dependency on phico/query

--- src/DB.php
+++ src/DB.php
@@ -7,7 +7,6 @@ namespace Phico\Database;
 use PDO;
 use PDOException;
 use PDOStatement;
-use Phico\Query\Query;
 use Throwable;
 
 
@@ -123,41 +122,6 @@ class DB
 
             throw $e;
         }
-    }
-    /**
-     * Returns a single row as an object, or null if no rows found
-     * @param \Phico\Query\Query $query The query to execute
-     * @return null|object
-     */
-    public function fetchOne(Query $query): ?object
-    {
-        $query->limit(1);
-        $stmt = $this->execute($query->toSql($this->driver), $query->getParams());
-        $row = $stmt->fetch(PDO::FETCH_OBJ);
-
-        return (false === $row) ? null : $row;
-    }
-    /**
-     * Returns an array of matching rows, or an empty array if no rows found
-     * @param \Phico\Query\Query $query The query to execute
-     * @return array
-     */
-    public function fetchMany(Query $query): array
-    {
-        $stmt = $this->execute($query->toSql($this->driver), $query->getParams());
-        $rows = $stmt->fetchAll(PDO::FETCH_OBJ);
-
-        return (false === $rows) ? [] : $rows;
-    }
-    /**
-     * Prepares and executes a Query instance safely
-     * @param Query $query The Query instance
-     * @return PDOStatement
-     * @throws DatabaseException
-     */
-    public function query(Query $query): PDOStatement
-    {
-        return $this->execute($query->toSql($this->driver), $query->getParams());
     }
     /**
      * Executes a raw SQL query using unsafe input and can execute multiple queries
