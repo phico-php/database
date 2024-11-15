bf22c6853efedd88252f932b09731286c0604801
indgy <65541311+indgy@users.noreply.github.com>
2024-11-15 11:52:44 +0000

    Update helpers with new method signatures

--- src/functions.php
+++ src/functions.php
@@ -2,30 +2,10 @@
 
 declare(strict_types=1);
 
-use Phico\Database\DatabaseException;
-
 if (!function_exists('db')) {
-    // db requires the name of the connection to use
-    function db(string $conn = null): \Phico\Database\DB
+    function db(?string $name = null, ?array $config = null): \Phico\Database\Database
     {
-        // fetch default connection name if not provided
-        $conn = (is_null($conn)) ? config()->get("database.use") : $conn;
-
-        // fetch connection details
-        $config = config()->get("database.connections.$conn");
-
-        // create dsn
-        $dsn = ($config['driver'] === 'sqlite')
-            ? sprintf('%s:%s', $config['driver'], path($config['database']))
-            : sprintf('%s:host=%s;port=%s;dbname=%s', $config['driver'], $config['host'], $config['port'], $config['database']);
-
-        // try to create PDO connection using config details
-        try {
-            $pdo = new PDO($dsn, $config['username'] ?? '', $config['password'] ?? '', $config['options'] ?? []);
-            return new \Phico\Database\DB($pdo);
-        } catch (PDOException $e) {
-            throw new DatabaseException('Failed to connect to database %s', 1005, null, [], [], $e);
-        }
+        return new \Phico\Database\Database($name, $config);
     }
 }
 if (!function_exists('table')) {
